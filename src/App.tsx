4565e8536b9f970e44d088c9468f99053e99d3e5
Gerardo Lopez Jr <gerardolopezjr1178@gmail.com>
2025-09-24 03:36:36 -0700

    Deploy Settings
    Added deployment github settings.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* IMPORTANT for GitHub Pages pathing: served from /Lantic */}
+      <BrowserRouter basename="/Lantic">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
